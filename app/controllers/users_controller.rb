f0ab1c0b42f5996c63300978bd1711dde98238ad
Arun Umapathy <arunumap@gmail.com>
2013-12-18 15:13:57 -0500

    progress bar now works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ before_action :authenticated!, :current_user
 	def show
 		@user = User.find(params[:id])
 		@measurement = Measurement.where(user_id: @user.id).first
+		@steps = (@measurement.steps_taken.to_f / @measurement.steps_goal.to_f)*100
 	end
 
 end
