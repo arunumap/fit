4ff9bfd75fb525d2dc0d72be945554e786d3d214
Arun Umapathy <arunumap@gmail.com>
2013-12-19 14:54:00 -0500

    fixed progress bar width

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,9 @@ before_action :authenticated!, :current_user
 		@distance = (@measurement.distances_total_distance.to_f / @measurement.distance_goal.to_f)*100
 		@calories = (@measurement.calories_out.to_f / @measurement.calories_out_goal.to_f)*100
 		@minutes = (@measurement.veryActive_min.to_f / @measurement.active_minutes_goal.to_f)*100
+		if @minutes > 100
+			@minutes = 100
+		end
 	end
 
 end
